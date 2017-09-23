3d52a867b23564c933f58a9d13948658e3b69ae5
IainMethe <methe.iain@gmail.com>
2017-09-22 21:47:38 -0400

    Started linking configurations' queries to actual database queries

--- chef/Query.py
+++ chef/Query.py
@@ -1,4 +1,5 @@
 import abc
+from food_db_manager import db_user
 
 class Query(abc.ABC):
 
@@ -31,10 +32,23 @@ class NextOccurrenceQuery(Query):
         self.food = food
         self.location = location
         self.time = time
+        self.user = db_user()
 
 
     def apply(self, configuration) -> str:
-        configuration.send_message("Next: " + self.food + ", " + self.location + ", " + self.time)
+        answer = self.db_user.get_next_occurances(self.food)
+        message = ""
+        if not answer:
+            message = "Sorry, we couldn't find anything like" + self.food + "in our database :("
+        else:
+            meal = answer[0][0]
+            food_item = answer[0][1]
+            self.location = meal.d_hall
+            self.time = meal.name
+            message = "Next: " + self.food + ", " + self.location + ", " + self.time
+
+        configuration.send_message(message)
+        return message
 
 
 
@@ -43,7 +57,21 @@ class CurrentStatusQuery(Query):
     def __init__(self, food, location=""):
         self.food = food
         self.location = location
+        self.user = db_user()
 
     def apply(self, configuration) -> str:
-        configuration.send_message("Now: " + self.food + ", " + self.location)
+        answer = self.user.get_current_status()
+        message = ""
+        if not answer:
+            message = "Sorry, we couldn't find anything :("
+        else:
+            meal = answer[0][0]
+            food_item = answer[0][1]
+            self.location = meal.d_hall
+            self.time = meal.name
+            message = "Now: " + self.food + ", " + self.location
+
+        configuration.send_message(message)
+        return message
+        #configuration.send_message("Now: " + self.food + ", " + self.location)
 
--- food_db_manager.py
+++ food_db_manager.py
@@ -9,8 +9,10 @@ import requests, datetime, json
 
 Base = declarative_base()
 
+#Represents an object thats connected to the database with certain settings
 class db_connection():
-    def __init__(self):
+    def __init__(self, db_name):
+        self.db_name = db_name
         self.engine = self.create_engine()
         self.sess = sessionmaker(bind=self.engine)()
         Base.metadata.create_all(self.engine)
@@ -19,14 +21,14 @@ class db_connection():
         #eng_string='''sqlite:///my_neu_chef.db'''
         #code for mysql connection
         eng_string='''mysql+mysqlconnector://{0[userName]}:{0[password]}@
-        {0[serverName]}:{0[portNumber]}/{0[dbName]}'''.format(dbcs)
+        {0[serverName]}:{0[portNumber]}/{1}'''.format(dbcs, self.db_name)
         eng=create_engine(eng_string)
         return eng
 
-
+#Represents a database administrator who can add menus to the database
 class db_admin(db_connection):
-    def __init__(self):
-        super(db_admin, self).__init__()
+    def __init__(self, db_name="my_neu_chef"):
+        super(db_admin, self).__init__(db_name)
         self.dining_halls_dict = {
         "Stetson West":"586d05e4ee596f6e6c04b528",
         "Stetson East":"586d05e4ee596f6e6c04b527",
@@ -35,6 +37,7 @@ class db_admin(db_connection):
         self.site_id = "5751fd2b90975b60e048929a"
         self.base_url = "https://new.dineoncampus.com/v1/location/menu.json"
 
+    #add menus from all dining hall locations for the given day
     def add_all_menus_for_day(self, date=datetime.date.today()):
         print('Getting all dining hall menus for', date)
 
@@ -53,6 +56,7 @@ class db_admin(db_connection):
 
         print('Done')
 
+    #add menus from all dining hall locations for the next week, starting today
     def add_all_menus_for_next_week(self):
         td = datetime.timedelta(days=1)
         d = datetime.date.today()
@@ -60,6 +64,7 @@ class db_admin(db_connection):
             self.add_all_menus_for_day(d)
             d+=td
 
+    #add menus from all dining hall locations for the next 30 days, starting today
     def add_all_menus_for_next_month(self):
         td = datetime.timedelta(days=1)
         d = datetime.date.today()
@@ -67,6 +72,7 @@ class db_admin(db_connection):
             self.add_all_menus_for_day(d)
             d+=td
 
+    #add the given menu data/given dining hall/given day to the db
     def add_menu_data(self, m_data, d_hall_name, m_date):
         for period in m_data['periods']:
             #for each meal create a unique meal id and meal object
@@ -88,6 +94,7 @@ class db_admin(db_connection):
             self.sess.merge(m)
         self.sess.commit()
 
+    #retrieve data from the online menu api for the given dining hall/date
     def get_data_from_api(self, d_hall, date=datetime.date.today()):
         #create request arguments
         #date=date.strftime("%Y-%m-%d")
@@ -105,12 +112,15 @@ class db_admin(db_connection):
 
         return data
 
-
+#Represents a database user who can query the database 
 class db_user(db_connection):
-    def __init__(self):
-        super(db_user, self).__init__()
+    def __init__(self, db_name="my_neu_chef"):
+        super(db_user, self).__init__(db_name)
 
-    def get_next_occurances(self, food, d_halls=['IV', 'Stetson West', 'Stetson East'], max_occurances=5):
+    #TODO: check the date and time as part of the query
+    #get the next occurances of the given food in the given dining halls
+    def get_next_occurances(self, food, d_halls=['IV', 'Stetson West', 
+        'Stetson East'], max_occurances=5):
         #create a query of meal and item colums
         q = self.sess.query(Meal, Food_Item).join(Food_Item.Meals)
         #filter for items like the one the user wants
@@ -118,12 +128,19 @@ class db_user(db_connection):
         q = q.filter(Food_Item.name.like(f_string))
         #filter for dining halls the user wants
         q = q.filter(Meal.d_hall.in_(d_halls))
+        #return only meals from today on
+        q = q.filter(Meal.date >= datetime.date.today())
         #order the results by date, then by meal
         q = q.order_by(Meal.date).order_by(Meal.name)
 
         return q.all()[:max_occurances]
 
+    def get_current_status(self, somethingelse):
+        #not too sure what this should be yet
+        print("Get current status unimplemented")
+        return []
 
+#Represents a meal with a date, location, name and items
 class Meal(Base):
     __tablename__ = 'Meal'
 
@@ -132,10 +149,10 @@ class Meal(Base):
     d_hall = Column(String(50))
     name = Column(Enum('Breakfast', 'Lunch', 'Dinner'))
 
+    #table for linking meals and their food items in a many-many relationship
     assoc_t = Table('Meal_Food_Item_assoc', Base.metadata,
             Column('Meal_id', String(50), ForeignKey('Meal.id')),
             Column('Food_Item_id', String(50), ForeignKey('Food_Item.id')))
-
     Food_Items = relationship('Food_Item',
         secondary=assoc_t,
         backref='Meals')
@@ -144,7 +161,7 @@ class Meal(Base):
         return "<Meal(date='%s', d_hall='%s', name='%s')>"%(
             self.date, self.d_hall, self.name)
 
-
+#Represents a food item with a name, calorie count, and filters
 class Food_Item(Base):
     __tablename__ = 'Food_Item'
 
@@ -152,10 +169,10 @@ class Food_Item(Base):
     name = Column(String(150))
     calories = Column(Integer)
 
+    #table for linking food items and filters in a many-many relationship
     assoc_t = Table('Food_Item_Filter_assoc', Base.metadata,
             Column('Food_Item_id', String(50), ForeignKey('Food_Item.id')),
             Column('Filter_id', String(50), ForeignKey('Filter.id')))
-
     Filters = relationship('Filter',
         secondary=assoc_t,
         backref='Food_Items')
@@ -164,7 +181,7 @@ class Food_Item(Base):
         return "<Food_Item(name='%s', calories='%s')>"%(
             self.name, self.calories)
 
-
+#represents an item filter with a name
 class Filter(Base):
     __tablename__ = 'Filter'
 
