90ed53a1ed3a25f7405faebf655a1ec4e8bb6e8b
Aris Poloway <agpoloway@gmail.com>
2017-09-28 23:05:33 -0400

    Reworked thread spawning for event loop

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import settings
 from chef import DiscordDaemon
 from chef.Scheduler import Scheduler
 from chef.configuration.ConfigurationSettings import ConfigurationSettings
+import asyncio
 
 
 ConfigurationSettings.initialize_channels()
 Scheduler.initialize()
 
 
+def asyncio_wrapper_thread(event_loop, func, args):
+    asyncio.set_event_loop(event_loop)
+    func(args)
+
+
 def launch_daemon_thread(func, args):
-    t = threading.Thread(target=func, args=args)
+    t = threading.Thread(target=asyncio_wrapper_thread, args=(asyncio.get_event_loop(), func) + args)
     t.daemon = True
     t.start()
     return t
