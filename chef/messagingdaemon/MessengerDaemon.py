18501a2aaaa045c0cb4f87e5d119a9ee75c1cd88
Aris Poloway <agpoloway@gmail.com>
2017-09-29 19:04:54 -0400

    Changed messenger to automatically register on first message

--- chef/messagingdaemon/MessengerDaemon.py
+++ chef/messagingdaemon/MessengerDaemon.py
@@ -11,14 +11,12 @@ callbacks = {}
 # Subclass fbchat.Client and override required methods
 class MessengerBot(Client):
     def onMessage(self, author_id, message, thread_id, thread_type, **kwargs):
-        if thread_id in callbacks:
-            callbacks[thread_id].receive_new_message(message)
-        else:
-            if message.startswith("!register"):
-                configuration_settings = ConfigurationSettings.create_new_settings(MessengerConfiguration.get_configuration_type(), thread_id)
-                configuration_settings.set("messenger-thread-id", thread_id)
-                configuration_settings.set("messenger-thread-type", thread_type)
-                ConfigurationCreator.create_configuration_from_settings(configuration_settings)
+        if thread_id not in callbacks:
+            configuration_settings = ConfigurationSettings.create_new_settings(MessengerConfiguration.get_configuration_type(), thread_id)
+            configuration_settings.set("messenger-thread-id", thread_id)
+            configuration_settings.set("messenger-thread-type", thread_type)
+            ConfigurationCreator.create_configuration_from_settings(configuration_settings)
+        callbacks[thread_id].receive_new_message(message)
 
         self.markAsDelivered(author_id, thread_id)
         self.markAsRead(author_id)
